c34bdd9ab5966a4221fc82d0ecbbee6a92475f63
Remco Weekers <Remco.Weekers@gmail.com>
2013-07-26 21:41:50 +0200

    JDBC testje

--- quooc/app/controllers/Application.java
+++ quooc/app/controllers/Application.java
@@ -2,13 +2,33 @@ package controllers;
 
 import play.*;
 import play.mvc.*;
+import play.db.*;
+import java.sql.*;
 
 import views.html.*;
 
 public class Application extends Controller {
 
   public static Result index() {
-    System.out.println("Blablabla");
+    System.out.println("Blablabla3");
+    try {
+      Connection connection = DB.getConnection();
+      Statement statement = connection.createStatement();
+
+      String query = "select artist, title from songs";
+      ResultSet rs = statement.executeQuery(query);
+      while(rs.next()){
+	  	  String artist = rs.getString("artist");
+		  String title = rs.getString("title");
+		  System.out.println(artist + " - " + title);
+	  }
+
+	  connection.close();
+
+    } catch (SQLException se) {
+			System.out.println(se.getMessage());
+	}
+
     return ok(index.render("Your new application is ready."));
   }
 
