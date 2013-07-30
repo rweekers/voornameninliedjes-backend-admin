5addbe52562df2875dd0cc41fff762cedb311c65
Remco Weekers <Remco.Weekers@gmail.com>
2013-07-30 16:59:01 +0200

    Pagina aangemaakt.

--- quooc/app/controllers/Application.java
+++ quooc/app/controllers/Application.java
@@ -4,32 +4,27 @@ import play.*;
 import play.mvc.*;
 import play.db.*;
 import java.sql.*;
+import play.data.*;
+import models.*;
 
 import views.html.*;
 
 public class Application extends Controller {
 
+  static Form<Song> songForm = form(Song.class);
+
   public static Result index() {
-    System.out.println("Blablabla3");
-    try {
-      Connection connection = DB.getConnection();
-      Statement statement = connection.createStatement();
+    return redirect(routes.Application.songs());
+  }
 
-      String query = "select artist, title from songs";
-      ResultSet rs = statement.executeQuery(query);
-      while(rs.next()){
-	  	  String artist = rs.getString("artist");
-		  String title = rs.getString("title");
-		  System.out.println(artist + " - " + title);
-	  }
+    public static Result songs() {
+	  return ok(
+	    views.html.index.render(Song.all(), songForm)
+	  );
+}
 
-	  connection.close();
-
-    } catch (SQLException se) {
-			System.out.println(se.getMessage());
-	}
-
-    return ok(index.render("Your new application is ready."));
+    public static Result newSong() {
+      return TODO;
   }
 
 }
