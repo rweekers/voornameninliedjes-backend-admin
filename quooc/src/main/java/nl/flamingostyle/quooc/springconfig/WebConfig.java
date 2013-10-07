61d76128e638ec7570f6c0f1eaa8a15c87b0c3b5
Remco Weekers <Remco.Weekers@gmail.com>
2013-10-07 22:51:58 +0200

    Added jps's.

--- quooc/src/main/java/nl/flamingostyle/quooc/springconfig/WebConfig.java
+++ quooc/src/main/java/nl/flamingostyle/quooc/springconfig/WebConfig.java
@@ -14,70 +14,87 @@ import org.springframework.orm.hibernate4.HibernateTransactionManager;
 import org.springframework.orm.hibernate4.LocalSessionFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
- 
+
 @Configuration
 @EnableWebMvc
-@ComponentScan(basePackages="nl.flamingostyle.quooc")
+@ComponentScan(basePackages = "nl.flamingostyle.quooc")
 @EnableTransactionManagement
-public class WebConfig {
- 
-	private static final String PROPERTY_NAME_DATABASE_DRIVER = "com.mysql.jdbc.Driver";  
-    private static final String PROPERTY_NAME_DATABASE_PASSWORD = "root";  
-    private static final String PROPERTY_NAME_DATABASE_URL = "jdbc:mysql://localhost/voornaaminliedje";  
-    private static final String PROPERTY_NAME_DATABASE_USERNAME = "root";  
-      
-    private static final String PROPERTY_NAME_HIBERNATE_DIALECT = "org.hibernate.dialect.MySQL5InnoDBDialect";  
-    private static final String PROPERTY_NAME_HIBERNATE_SHOW_SQL = "true";
-    private static final String PROPERTY_NAME_ENTITYMANAGER_PACKAGES_TO_SCAN = "nl.flamingostyle.quooc.domain";
-    private static final String PROPERTY_NAME_HIBERNATE_HBM2DDL = "hibernate.hbm2ddl.validate";
+public class WebConfig extends WebMvcConfigurerAdapter {
 
-    @Resource  
-    private Environment env;
-    
-    @Bean
-    public InternalResourceViewResolver viewResolver() {
-        InternalResourceViewResolver resolver = 
-                    new InternalResourceViewResolver();
-        resolver.setPrefix("/WEB-INF/view/");
-        resolver.setSuffix(".jsp");
-        return resolver;
-    }
+	private static final String PROPERTY_NAME_DATABASE_DRIVER = "com.mysql.jdbc.Driver";
+	private static final String PROPERTY_NAME_DATABASE_PASSWORD = "root";
+	private static final String PROPERTY_NAME_DATABASE_URL = "jdbc:mysql://localhost/voornaaminliedje";
+	private static final String PROPERTY_NAME_DATABASE_USERNAME = "root";
 
-    @Bean  
-    public DataSource dataSource() {  
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();  
-          
-        dataSource.setDriverClassName(PROPERTY_NAME_DATABASE_DRIVER);  
-        dataSource.setUrl(PROPERTY_NAME_DATABASE_URL);  
-        dataSource.setUsername(PROPERTY_NAME_DATABASE_USERNAME);  
-        dataSource.setPassword(PROPERTY_NAME_DATABASE_PASSWORD);  
-          
-        return dataSource;  
-    }  
-    
-    @Bean(name="sessionFactory") 
-    public LocalSessionFactoryBean sessionFactory() {  
-        LocalSessionFactoryBean sessionFactoryBean = new LocalSessionFactoryBean();  
-        sessionFactoryBean.setPackagesToScan(PROPERTY_NAME_ENTITYMANAGER_PACKAGES_TO_SCAN);
-        sessionFactoryBean.setDataSource(dataSource());  
-        sessionFactoryBean.setHibernateProperties(hibProperties());  
-        return sessionFactoryBean;  
-    } 
-    
-    private Properties hibProperties() {  
-        Properties properties = new Properties();  
-        properties.put(PROPERTY_NAME_HIBERNATE_DIALECT, PROPERTY_NAME_HIBERNATE_DIALECT);  
-        properties.put(PROPERTY_NAME_HIBERNATE_SHOW_SQL, PROPERTY_NAME_HIBERNATE_SHOW_SQL);
-        properties.put(PROPERTY_NAME_HIBERNATE_HBM2DDL, PROPERTY_NAME_HIBERNATE_HBM2DDL);
-        return properties;    
-    } 
-    
-    @Bean
-    public HibernateTransactionManager transactionManager() {
-        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
-        transactionManager.setSessionFactory(sessionFactory().getObject());
-        return transactionManager;
-    }
-     
+	private static final String PROPERTY_NAME_HIBERNATE_DIALECT = "org.hibernate.dialect.MySQL5InnoDBDialect";
+	private static final String PROPERTY_NAME_HIBERNATE_SHOW_SQL = "true";
+	private static final String PROPERTY_NAME_ENTITYMANAGER_PACKAGES_TO_SCAN = "nl.flamingostyle.quooc.domain";
+	private static final String PROPERTY_NAME_HIBERNATE_HBM2DDL = "hibernate.hbm2ddl.validate";
+
+	@Resource
+	private Environment env;
+
+	/**
+	 * Since we don't have any controller logic, simpler to just define
+	 * controller for page using View Controller. Note: had to extend
+	 * WebMvcConfigurerAdapter to get this functionality
+	 * 
+	 * @param registry
+	 */
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/").setViewName("home");
+	}
+
+	@Bean
+	public InternalResourceViewResolver viewResolver() {
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setPrefix("/WEB-INF/view/");
+		resolver.setSuffix(".jsp");
+		return resolver;
+	}
+
+	@Bean
+	public DataSource dataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+
+		dataSource.setDriverClassName(PROPERTY_NAME_DATABASE_DRIVER);
+		dataSource.setUrl(PROPERTY_NAME_DATABASE_URL);
+		dataSource.setUsername(PROPERTY_NAME_DATABASE_USERNAME);
+		dataSource.setPassword(PROPERTY_NAME_DATABASE_PASSWORD);
+
+		return dataSource;
+	}
+
+	@Bean(name = "sessionFactory")
+	public LocalSessionFactoryBean sessionFactory() {
+		LocalSessionFactoryBean sessionFactoryBean = new LocalSessionFactoryBean();
+		sessionFactoryBean
+				.setPackagesToScan(PROPERTY_NAME_ENTITYMANAGER_PACKAGES_TO_SCAN);
+		sessionFactoryBean.setDataSource(dataSource());
+		sessionFactoryBean.setHibernateProperties(hibProperties());
+		return sessionFactoryBean;
+	}
+
+	private Properties hibProperties() {
+		Properties properties = new Properties();
+		properties.put(PROPERTY_NAME_HIBERNATE_DIALECT,
+				PROPERTY_NAME_HIBERNATE_DIALECT);
+		properties.put(PROPERTY_NAME_HIBERNATE_SHOW_SQL,
+				PROPERTY_NAME_HIBERNATE_SHOW_SQL);
+		properties.put(PROPERTY_NAME_HIBERNATE_HBM2DDL,
+				PROPERTY_NAME_HIBERNATE_HBM2DDL);
+		return properties;
+	}
+
+	@Bean
+	public HibernateTransactionManager transactionManager() {
+		HibernateTransactionManager transactionManager = new HibernateTransactionManager();
+		transactionManager.setSessionFactory(sessionFactory().getObject());
+		return transactionManager;
+	}
+
 }
