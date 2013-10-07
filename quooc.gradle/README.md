fa2454d84b5ae9e1cfd8d42757d7b4b15d489a4c
Remco Weekers <Remco.Weekers@gmail.com>
2013-10-07 22:53:00 +0200

    Removed old project.

--- quooc.gradle/README.md
+++
@@ -1,4 +0,0 @@
-spring-mvc-ajax
-========================
-
-Sample Spring MVC Application demonstrating JSON AJAX requests with jQuery front-end and Spring MVC back-end.
--- quooc.gradle/src/main/java/nl/flamingostyle/quooc/service/SongService.java
+++
@@ -1,9 +0,0 @@
-package nl.flamingostyle.quooc.service;
-
-import nl.flamingostyle.quooc.domain.Song;
-
-public interface SongService {
-	public Song getRandom();
-	public Song getById(Long id);
-	public void save(Song song);
-}
