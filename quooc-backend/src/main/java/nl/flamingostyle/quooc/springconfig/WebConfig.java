c81306784c417b75ef69b30fc93272f13447cb4d
Remco Weekers <Remco.Weekers@gmail.com>
2013-10-15 10:29:49 +0200

    Switch to postgresql.

--- quooc-backend/src/main/java/nl/flamingostyle/quooc/springconfig/WebConfig.java
+++ quooc-backend/src/main/java/nl/flamingostyle/quooc/springconfig/WebConfig.java
@@ -2,13 +2,11 @@ package nl.flamingostyle.quooc.springconfig;
 
 import java.util.Properties;
 
-import javax.annotation.Resource;
 import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.hibernate4.HibernateTransactionManager;
 import org.springframework.orm.hibernate4.LocalSessionFactoryBean;
@@ -24,20 +22,19 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 public class WebConfig extends WebMvcConfigurerAdapter {
 
 	private static final String PROPERTY_NAME_DATABASE_DRIVER = "org.postgresql.Driver";
+        // private static final String PROPERTY_NAME_DATABASE_DRIVER = "com.mysql.jdbc.Driver";
 	private static final String PROPERTY_NAME_DATABASE_PASSWORD = "root";
 	// private static final String PROPERTY_NAME_DATABASE_URL = "jdbc:mysql://localhost/voornaaminliedje";
         private static final String PROPERTY_NAME_DATABASE_URL = "jdbc:postgresql://localhost:5432/voornaaminliedje";
 	private static final String PROPERTY_NAME_DATABASE_USERNAME = "postgres";
+        // private static final String PROPERTY_NAME_DATABASE_USERNAME = "root";
 
-	// private static final String PROPERTY_NAME_HIBERNATE_DIALECT = "org.hibernate.dialect.PostgreSQLDialect";
-        private static final String PROPERTY_NAME_HIBERNATE_DIALECT = "net.sf.hibernate.dialect.PostgreSQLDialect";
+	private static final String PROPERTY_NAME_HIBERNATE_DIALECT = "org.hibernate.dialect.PostgreSQLDialect";
+        // private static final String PROPERTY_NAME_HIBERNATE_DIALECT = "org.hibernate.dialect.MySQLInnoDBDialect";
 	private static final String PROPERTY_NAME_HIBERNATE_SHOW_SQL = "true";
 	private static final String PROPERTY_NAME_ENTITYMANAGER_PACKAGES_TO_SCAN = "nl.flamingostyle.quooc.domain";
 	private static final String PROPERTY_NAME_HIBERNATE_HBM2DDL = "hibernate.hbm2ddl.validate";
 
-	@Resource
-	private Environment env;
-	
 	@Bean
 	public InternalResourceViewResolver viewResolver() {
 		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
@@ -52,6 +49,7 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 
 		dataSource.setDriverClassName(PROPERTY_NAME_DATABASE_DRIVER);
 		dataSource.setUrl(PROPERTY_NAME_DATABASE_URL);
+                
 		dataSource.setUsername(PROPERTY_NAME_DATABASE_USERNAME);
 		dataSource.setPassword(PROPERTY_NAME_DATABASE_PASSWORD);
 
