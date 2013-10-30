6562334feb150ca925ce7b1ff12d0df2c6dc1bda
Remco Weekers <Remco.Weekers@gmail.com>
2013-10-30 12:53:59 +0100

    Update for searching on page.

--- quooc-frontend/webapp/resources/js/databinding.js
+++ quooc-frontend/webapp/resources/js/databinding.js
@@ -1,5 +1,5 @@
-var offset = 730;
-var max = 10;
+var offset = 0;
+var max = 15;
 
 function Song(data) {
     this.artist = ko.observable(data.artist);
@@ -9,18 +9,21 @@ function Song(data) {
 function SongsViewModel() {
     // Data
     var self = this;
-    
-    self.van = ko.observable(9);
-    self.tot = ko.observable(13);
 
+    self.voornaam = ko.observable("bert");
     self.songs = ko.observableArray([]);
+    self.findSongs = ko.observableArray([]);
+
     self.newSongText = ko.observable();
     self.url = ko.observable();
 
     url = "http://localhost:8080/voornaaminliedje/api/songs/some?offset=" + offset + "&max=" + max;
     urlMax = "http://localhost:8080/voornaaminliedje/api/songs/max";
     count = 0;
-    
+
+    urlFind = "http://localhost:8080/voornaaminliedje/api/song/find/";
+    +self.voornaam();
+
     $.getJSON(urlMax, function(data) {
         count = data.valueOf();
     });
@@ -33,12 +36,21 @@ function SongsViewModel() {
         self.songs(mappedSongs);
     });
 
+    // Load find from server, convert it to Song instances, then populate self.songs
+    $.getJSON(urlFind + self.voornaam(), function(allData) {
+        var mappedSongs = $.map(allData, function(item) {
+            return new Song(item);
+        });
+        self.findSongs(mappedSongs);
+    });
+
     self.previous = function() {
         if (offset > max) {
             offset = offset - max;
         } else {
             offset = 0;
         }
+
         url = "http://localhost:8080/voornaaminliedje/api/songs/some?offset=" + offset + "&max=" + max;
         $.getJSON(url, function(allData) {
             var mappedSongs = $.map(allData, function(item) {
@@ -46,14 +58,10 @@ function SongsViewModel() {
             });
             self.songs(mappedSongs);
         });
-        
-        self.van = offset;
-        self.tot = offset + max;
-        console.log("Waarden: " + self.van + " en " + self.tot);
     };
 
     self.next = function() {
-        if (offset + 2 * max < count){
+        if (offset + 2 * max < count) {
             offset = offset + max;
         } else {
             offset = count - max;
@@ -65,11 +73,15 @@ function SongsViewModel() {
             });
             self.songs(mappedSongs);
         });
-        
-        self.van = offset;
-        self.tot = offset + max;
-        
-        console.log("Waarden: " + self.van + " en " + self.tot);
+    };
+
+    self.zoeknummers = function() {
+        $.getJSON(urlFind + self.voornaam(), function(allData) {
+            var mappedSongs = $.map(allData, function(item) {
+                return new Song(item);
+            });
+            self.findSongs(mappedSongs);
+        });
     };
 }
 
