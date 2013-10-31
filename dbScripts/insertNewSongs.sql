144615f0233ccd5097e88abcd45643ee7015a93d
Remco Weekers <Remco.Weekers@gmail.com>
2013-10-31 09:44:27 +0100

    Added scripts for songs.

--- dbScripts/insertNewSongs.sql
+++ dbScripts/insertNewSongs.sql
@@ -1,2 +1,8 @@
 insert into song(artist, title, firstname, name_index, name_length, date_inserted, user_inserted, background)
 values('The Velvet Underground', 'Stephanie Says', 'Stephanie', 0, 9, current_timestamp, 'Remco', '''Stephanie Says'' is een nummer van ''The Velvet Underground'', oorspronkelijk opgenomen in 1968. Het is pas gereleased in 1985 in een geremixte versie. De oorspronkelijke versie staat op het album Velvet Underground compilation album uit 2005. In 2001 is het nummer gebruikt in de film ''The Royal Tenenbaums'' van Wes Anderson. ');
+
+insert into song(artist, title, firstname, name_index, name_length, date_inserted, user_inserted, background)
+values('Richard Berry', 'Louie Louie', 'Louie', 0, 5, current_timestamp, 'Remco', '''Louie Louie'' is een Amerikaans rock ''n'' roll nummer, geschreven voor Richard Berry in 1955. De meest bekende versie is de versie uit 1963 van The Kingsmen. Oorspronkelijk is het numer geschreven en uitgevoerd als Jamaicaanse ballad. ');
+
+insert into song(artist, title, firstname, name_index, name_length, date_inserted, user_inserted, background)
+values('The Kingsmen', 'Louie Louie', 'Louie', 0, 5, current_timestamp, 'Remco', 'Het nummer is oorspronkelijk geschreven door Richard Berry, maar de versie van The Kingsmen is het meest bekend. Deze versie is aan een FBI onderzoek onderworpen geweest, naar aanleiding van veronderstelde obscene teksten, maar bleef zonder gevolgen. Het nummer is zeer invloedrijk voor de rock ''n'' roll geweest.');
