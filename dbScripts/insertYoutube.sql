9e760fd6a5e0a1e6d158b430da28a06c1545d4db
Remco Weekers <Remco.Weekers@gmail.com>
2013-11-06 16:31:44 +0100

    Neaten dbsccripts.

--- dbScripts/insertYoutube.sql
+++
@@ -1,1 +0,0 @@
-update song set youtube = '<iframe width="320" height="180" src="//www.youtube.com/embed/uq-gYOrU8bA?feature=player_detailpage"></iframe>' where id = 12070;
--- dbScripts/sequence.sql
+++
@@ -1,2 +0,0 @@
-CREATE SEQUENCE song_id_seq;
-SELECT setval('song_id_seq', 12824);  
