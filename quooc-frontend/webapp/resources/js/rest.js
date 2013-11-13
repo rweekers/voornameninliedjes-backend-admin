b0b39bea0631b0b1dfa7c990c8d33e5892182216
Remco Weekers <Remco.Weekers@gmail.com>
2013-11-13 09:02:50 +0100

    Next step in logging.

--- quooc-frontend/webapp/resources/js/rest.js
+++ quooc-frontend/webapp/resources/js/rest.js
@@ -23,7 +23,7 @@ $(document).ready(function() {
     });
 
     logVisit();
-    // storeVisit();
+    storeVisit();
 });
 
 $(function() {
@@ -49,29 +49,5 @@ function callFlickr(tags) {
                 return false;
             }
         });
-    });
-}
-
-function logVisit() {
-
-    console.log("logging visit...");
-    var geoipUrl = "http://api.hostip.info/get_json.php";
-
-    $.getJSON(geoipUrl, function(data) {
-        console.log('Your IP Address is ' + data.ip);
-        console.log('Your country is ' + data.country_name);
-        console.log('Your city is ' + data.city);
-        console.log('Your browser is ' + BrowserDetect.browser);
-        ipAddress = data.ip;
-    }).fail(function(error) {
-        console.log(error);
-    });
-}
-
-function storeVisit() {
-    url = "http://localhost:8080/voornaaminliedje/api/visit/add?ipAddress=" + ipAddress + "&browser=" + browser + "&operatingSystem=" + operatingSystem;
-
-    $.getJSON(url, function() {
-        console.log("Storing visit for ip " + ipAddress);
     });
 }
