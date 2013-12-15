8d144bf222ade5e52069be6db53eb302ca4572ba
Remco Weekers <Remco.Weekers@gmail.com>
2013-12-15 21:44:40 +0100

    iframe implementation for youtube

--- quooc-frontend/webapp/resources/js/controllers.js
+++ quooc-frontend/webapp/resources/js/controllers.js
@@ -12,7 +12,7 @@ songApp.controller('SongListCtrl', function($scope, $http) {
 
     $http.get('http://localhost:8080/voornaaminliedje/api/song/al').success(function(data) {
         $scope.songOfTheDay = data;
-        run($scope.songOfTheDay);
+        songOTD = $scope.songOfTheDay;
     });
 
     logVisit();
@@ -25,24 +25,3 @@ songApp.controller('SongListCtrl', function($scope, $http) {
      logSearchInstruction($scope.zoekOpdracht);
     }
 });
-
-function run(songOTD) {
-    /*
-     * Simple player embed
-     */
-    // The video to load.
-    var videoID = songOTD.youtube;
-    // Lets Flash from another domain call JavaScript
-    var params = {
-        allowScriptAccess: "always"
-    };
-    // The element id of the Flash embed
-    var atts = {
-        id: "ytPlayer"
-    };
-    // All of the magic handled by SWFObject (http://code.google.com/p/swfobject/)
-    swfobject.embedSWF("http://www.youtube.com/v/" + videoID + "?version=3&enablejsapi=1&playerapiid=player1",
-        "videoDiv", "380", "214", "9", null, null, params, atts);
-
-
-}
