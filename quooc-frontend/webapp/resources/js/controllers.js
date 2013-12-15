04f82e4efc3e2ce50e56043c6ff9229963664e1e
Remco Weekers <Remco.Weekers@gmail.com>
2013-12-15 12:53:34 +0100

    Work in progress in rewriting to angularjs. Added dynamic youtube call.

--- quooc-frontend/webapp/resources/js/controllers.js
+++ quooc-frontend/webapp/resources/js/controllers.js
@@ -3,15 +3,42 @@
 /* Controllers */
 
 var songApp = angular.module('songApp', []);
+var songOTD;
 
 songApp.controller('SongListCtrl', function($scope, $http) {
-  $http.get('http://localhost:8080/voornaaminliedje/api/songs/all').success(function(data) {
-    $scope.songs = data.splice(0,10);
-  });
+    $http.get('http://localhost:8080/voornaaminliedje/api/songs/all').success(function(data) {
+        $scope.songs = data.splice(0, 10);
+    });
 
-  $http.get('http://localhost:8080/voornaaminliedje/api/song/al').success(function(data) {
-    $scope.songOfTheDay = data;
-  });
+    $http.get('http://localhost:8080/voornaaminliedje/api/song/al').success(function(data) {
+        $scope.songOfTheDay = data;
+        run($scope.songOfTheDay);
+    });
 
-  $scope.orderProp = 'artist';
+    logVisit();
+
+    $scope.orderProp = 'artist';
 });
+
+function run(songOTD) {
+    /*
+     * Simple player embed
+     */
+    console.debug("Songofthe day is " + songOTD.youtube);
+    // The video to load.
+    // var videoID = "uq-gYOrU8bA"
+    var videoID = songOTD.youtube;
+    // Lets Flash from another domain call JavaScript
+    var params = {
+        allowScriptAccess: "always"
+    };
+    // The element id of the Flash embed
+    var atts = {
+        id: "ytPlayer"
+    };
+    // All of the magic handled by SWFObject (http://code.google.com/p/swfobject/)
+    swfobject.embedSWF("http://www.youtube.com/v/" + videoID + "?version=3&enablejsapi=1&playerapiid=player1",
+        "videoDiv", "380", "214", "9", null, null, params, atts);
+
+
+}
--- quooc-frontend/webapp/resources/js/rest.js
+++ quooc-frontend/webapp/resources/js/rest.js
@@ -11,6 +11,7 @@ var operatingSystem;
 $(document).ready(function() {
 
     // Random Song AJAX Request
+    /*
     $('#randomSong').click(function() {
         $.getJSON('http://localhost:8080/voornaaminliedje/api/song/all', function(song) {
             $('#songResponse').text(song.artist + ' - ' + song.title);
@@ -21,10 +22,14 @@ $(document).ready(function() {
             console.log(error);
         });
     });
+    */
 
-    logVisit();
+    // logVisit();
+    // google.load("swfobject", "2.1");
+    // _run();
 });
 
+/*
 $(function() {
     $.getJSON('http://localhost:8080/voornaaminliedje/api/song/all', function(song) {
         $('#songResponse').text(song.artist + ' - ' + song.title);
@@ -50,3 +55,23 @@ function callFlickr(tags) {
         });
     });
 }
+*/
+/*
+function _run() {
+        /*
+        * Simple player embed
+        */
+// console.debug("Songofthe day is " + songOfTheDay.title);
+// The video to load.
+/*        var videoID = "uq-gYOrU8bA"
+        // Lets Flash from another domain call JavaScript
+        var params = { allowScriptAccess: "always" };
+        // The element id of the Flash embed
+        var atts = { id: "ytPlayer" };
+        // All of the magic handled by SWFObject (http://code.google.com/p/swfobject/)
+        swfobject.embedSWF("http://www.youtube.com/v/" + videoID + "?version=3&enablejsapi=1&playerapiid=player1", 
+                           "videoDiv", "380", "214", "9", null, null, params, atts);
+        
+        
+      }*/
+// google.setOnLoadCallback(_run);*/
