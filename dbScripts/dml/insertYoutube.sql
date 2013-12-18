7da2bd9326b7d5b41834591e8a4a88b5d39aee2e
Remco Weekers <Remco.Weekers@gmail.com>
2013-12-18 16:27:41 +0100

    Updated youtube link.

--- dbScripts/dml/insertYoutube.sql
+++ dbScripts/dml/insertYoutube.sql
@@ -1,1 +1,1 @@
-update song set youtube = '<iframe width="320" height="180" src="//www.youtube.com/embed/uq-gYOrU8bA?feature=player_detailpage"></iframe>' where id = 12070;
+update song set youtube = 'uq-gYOrU8bA' where id = 12070;
