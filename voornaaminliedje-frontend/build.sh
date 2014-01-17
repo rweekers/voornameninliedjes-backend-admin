e9653ed2fed161e74b0dfe837069706b75fdc428
Remco Weekers <Remco.Weekers@gmail.com>
2014-01-17 09:14:08 +0100

    Added build script for frontend deploy.

---
+++ voornaaminliedje-frontend/build.sh
@@ -0,0 +1,12 @@
+rm -rf target
+mkdir target
+mkdir target/resources
+mkdir target/resources/js
+mkdir target/resources/js/libs
+cp webapp/resources/js/libs/browserDetect.js target/resources/js/libs
+cp webapp/resources/js/libs/angular.js target/resources/js/libs
+cp webapp/resources/js/log.js target/resources/js
+cp webapp/resources/js/services.js target/resources/js
+cp webapp/resources/js/app.js target/resources/js
+cp webapp/resources/js/controllers.js target/resources/js
+cp webapp/underConstruction.html target/index.html
