a55a7000220015fd554b8b2a2d0be74a8e7e1d5e
Remco Weekers <Remco.Weekers@gmail.com>
2014-01-19 16:02:38 +0100

    Update for csrf.

--- voornaaminliedje-frontend/webapp/resources/js/controllers.js
+++ voornaaminliedje-frontend/webapp/resources/js/controllers.js
@@ -19,6 +19,36 @@ function store($http, ipAddress, country, city) {
     var browser = BrowserDetect.browser + BrowserDetect.version;
     var operatingSystem = BrowserDetect.OS;
 
+
+    $(function() {
+        // var token = $("meta[name='_csrf']").attr("content");
+        // var header = $("meta[name='_csrf_header']").attr("content");
+        var token = 'blabla';
+        var header = '_csrf_header';
+        $(document).ajaxSend(function(e, xhr, options) {
+            xhr.setRequestHeader(header, token);
+        });
+    });
+
+    $.post("http://127.0.0.1:8080/voornaaminliedje/api/visit/add", {
+        ipAddress: ipAddress,
+        country: country,
+        city: city,
+        browser: browser,
+        operatingSystem: operatingSystem
+    }).done(function(data) {
+        console.log("Post succesful " + data);
+    })
+        .fail(function(data) {
+            console.log("Eror " + data.statusText);
+        });
+
+    // var token = $("meta[name='_csrf']").attr("content");
+    // var header = $("meta[name='_csrf_header']").attr("content");
+
+    // $http.defaults.headers.common['_csrf_header'] = token;
+
+    /*
     $http({
         url: 'http://127.0.0.1:8080/voornaaminliedje/api/visit/add',
         method: "POST",
@@ -29,8 +59,6 @@ function store($http, ipAddress, country, city) {
             browser: browser,
             operatingSystem: operatingSystem
         }
-    }).success(function(data) {
-    })
-        .error(function(data) {
-        });
+    }).success(function(data) {})
+        .error(function(data) {});*/
 }
