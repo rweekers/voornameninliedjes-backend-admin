dda8906ad12e20ee7e3ebf6070d274e3dd944166
Remco Weekers <Remco.Weekers@gmail.com>
2014-05-14 22:54:47 +0200

    Update for basicauth response. To fix.

---
+++ voornaaminliedje-backend/src/main/java/org/orangeflamingo/voornaaminliedje/controller/CustomBasicAuthenticationEntryPoint.java
@@ -0,0 +1,38 @@
+package org.orangeflamingo.voornaaminliedje.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
+
+/**
+ * Does not add header that requires a username and password via a popup
+ * 
+ * @author bchild
+ * 
+ */
+public class CustomBasicAuthenticationEntryPoint extends
+		BasicAuthenticationEntryPoint {
+
+	protected static Logger logger = Logger.getLogger("service");
+
+	public CustomBasicAuthenticationEntryPoint(String realmName) {
+		setRealmName(realmName);
+	}
+
+	@Override
+	public void commence(final HttpServletRequest request,
+			final HttpServletResponse response,
+			final AuthenticationException authException) throws IOException,
+			ServletException {
+		logger.info("CustomBasicAuthenticaition blabla...");
+		response.sendError(HttpServletResponse.SC_UNAUTHORIZED,
+				authException.getMessage());
+	}
+
+}
--- voornaaminliedje-backend/src/main/java/org/orangeflamingo/voornaaminliedje/springconfig/SecurityConfig.java
+++ voornaaminliedje-backend/src/main/java/org/orangeflamingo/voornaaminliedje/springconfig/SecurityConfig.java
@@ -1,26 +1,32 @@
 package org.orangeflamingo.voornaaminliedje.springconfig;
 
+import org.apache.log4j.Logger;
+import org.orangeflamingo.voornaaminliedje.controller.CustomBasicAuthenticationEntryPoint;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+	protected static Logger logger = Logger.getLogger("service");
+	
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth)
 			throws Exception {
 		auth.inMemoryAuthentication().withUser("admin")
 				.password("5095df0e6547e2647d5bc40f1ecd9afe").roles("ADMIN");
-		auth.inMemoryAuthentication().withUser("Remco").password("ccafbc2f4c5e0d2b262ff070476678b7")
-				.roles("ADMIN");
-		auth.inMemoryAuthentication().withUser("Nadja").password("82adbb3e824d6e62038273b8e6ac3eb4")
-		.roles("ADMIN");
+		auth.inMemoryAuthentication().withUser("Remco")
+				.password("ccafbc2f4c5e0d2b262ff070476678b7").roles("ADMIN");
+		auth.inMemoryAuthentication().withUser("Nadja")
+				.password("82adbb3e824d6e62038273b8e6ac3eb4").roles("ADMIN");
 	}
 
 	@Override
@@ -33,5 +39,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 				.antMatchers(HttpMethod.GET, "/api/visit/**").permitAll()
 				.antMatchers(HttpMethod.GET, "/api/admin/**").hasRole("ADMIN")
 				.and().httpBasic().and().csrf().disable();
+	}
+
+	@Bean
+	public BasicAuthenticationEntryPoint entryPoint() {
+		BasicAuthenticationEntryPoint basicAuthenticationEntryPoint = new CustomBasicAuthenticationEntryPoint(
+				"RealmName");
+		logger.info("bladiebladiebla.");
+		// basicAuthenticationEntryPoint.setRealmName("Basic WF Realm");
+		return basicAuthenticationEntryPoint;
 	}
 }
