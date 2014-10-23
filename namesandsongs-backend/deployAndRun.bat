3234ba65ce76dfe5911fa18d6a6975324ccf82fb
Remco Weekers <Remco.Weekers@gmail.com>
2014-10-23 15:10:11 +0200

    Deletd deployAndRun.bat script, because jetty:run is configured properly.

--- namesandsongs-backend/deployAndRun.bat
+++
@@ -1,4 +0,0 @@
-rm D:/Tools/jetty-distribution-8.1.15.v20140411/webapps/namesandsongs.war
-cp target/namesandsongs.war D:/Tools/jetty-distribution-8.1.15.v20140411/webapps
-cd $JETTY_HOME
-java -jar start.jar
