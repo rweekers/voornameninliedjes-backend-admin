89042d87553c0afaa194acaaa888fd1c391cb5d2
Remco Weekers <Remco.Weekers@gmail.com>
2014-10-27 11:41:39 +0100

    Updated menu with angular. Merged bootstrap code with angular.

--- frontend/app/app.js
+++ frontend/app/app.js
@@ -5,8 +5,16 @@ angular.module('myApp', [
   'ngRoute',
   'myApp.view1',
   'myApp.view2',
+  'myApp.about',
+  'myApp.contact',
+  'myApp.songs',
   'myApp.version'
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+}])
+.controller('HeaderCtrl', function($scope, $location) {
+    $scope.isActive = function(route) {
+        return route === $location.path();
+    }
+});
