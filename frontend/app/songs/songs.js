8bdefc59298387b2917427656976b12c115a0091
Remco Weekers <Remco.Weekers@gmail.com>
2014-11-03 15:36:37 +0100

    Updated songlist, first steps to serverside pagination.

--- frontend/app/songs/songs.js
+++ frontend/app/songs/songs.js
@@ -14,7 +14,11 @@ angular.module('myApp.songs', ['ngRoute', 'ngResource'])
 
         console.log('Locatie ' + $location.path());
 
-        $scope.songs = Song.query();
+        $scope.songs = Song.query({
+                                    count: 5,
+                                    page: 1,
+                                    filterArtist: 'Bob'
+        });
         // $scope.orderProp = 'ipAddress';
     }
 ])
@@ -24,8 +28,25 @@ angular.module('myApp.songs', ['ngRoute', 'ngResource'])
          return $resource('/namesandsongs/api/song/all', {}, {
              query: {
                  method: 'GET',
+                 params: {
+                     page: '',
+                     count: '',
+                     sortingArtist: '',
+                     sortingTitle: '',
+                     filterArtist: '',
+                     filterTitle: ''
+                 },
                  isArray: true
              }
          });
      }
  ]);
+
+/*
+                        count: $scope.tableParams.$params.count,
+                        page: $scope.tableParams.$params.page - 1,
+                        sortingArtist: $scope.tableParams.$params.sorting.artist,
+                        sortingTitle: $scope.tableParams.$params.sorting.title,
+                        filterArtist: $scope.tableParams.$params.filter.artist,
+                        filterTitle: $scope.tableParams.$params.filter.title
+*/
