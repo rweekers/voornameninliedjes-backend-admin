62da1c067bf080e243c7832c4d26b195fbf95ef1
Remco Weekers <Remco.Weekers@gmail.com>
2014-12-12 10:10:06 +0100

    Update for editing songs through remarks.

--- admin/app/remark/remark.js
+++ admin/app/remark/remark.js
@@ -11,26 +11,55 @@ angular.module('myApp.remark', ['ngRoute'])
     }
 ])
 
-.controller('RemarkCtrl', ['$scope', '$routeParams', 'Remark',
-    function($scope, $routeParams, Remark) {
+.controller('RemarkCtrl', ['$scope', '$routeParams', '$cookieStore', 'Remark', 'Song',
+    function($scope, $routeParams, $cookieStore, Remark, Song) {
         // $scope.remarks = Remark.query();
+        Remark.get({
+            id: $routeParams.remarkId
+        }).$promise.then(function(data) {
+            $scope.remark = data;
+            console.log('blabla' + data.song.id);
+            $scope.song = Song.get({
+                id: data.song.id
+            })
+        });;
+        /*
         $scope.remark = Remark.get({
             id: $routeParams.remarkId
-        });
+        }).then(function(data) {
+            console.log("Blabla " + data.song.id)
+            $scope.song = Song.get(data.song.id);
+            // user.abc = true;
+            // user.$save();
+        });*/
+        /*
+        Remark.get({
+            id: $routeParams.remarkId
+        })
+            .$promise.then(function(data) {
+                $scope.remark = data;
+                console.log("Blabla " + data.song);
+                $scope.song = data.song;
+                // $scope.user = user;
+            });*/
+        /*
+        $scope.song = Song.get({
+            id: 12743
+        });*/
 
         $scope.save = function() {
-            // console.log("Saving song by user " + $cookieStore.get('user'));
+            console.log("Saving song by user " + $cookieStore.get('user'));
             // $scope.song.userModified = $cookieStore.get('user');
             // $scope.song.$save();
             // $location.path('/songs');
-            console.log("Saving song " + $scope.remark.song.artist + " - " + $scope.remark.song.title);
-            $scope.remark.song.$save();
+            // console.log("Saving song " + $scope.remark.song.artist + " - " + $scope.remark.song.title);
+            $scope.song.$save();
         };
 
         $scope.cancel = function() {
-            console.log("Canceling...");
-            $location.path('/remarks');
+            // console.log("Canceling...");
+            // $location.path('/remarks');
         };
-        console.log("Opening remark details...");
+        // console.log("Opening remark details...");
     }
 ]);
