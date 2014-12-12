0ec57b52075294a2c8d8a8777c692738bbd9aabb
Remco Weekers <Remco.Weekers@gmail.com>
2014-12-12 10:13:54 +0100

    Cleaned up code.

--- admin/app/remark/remark.js
+++ admin/app/remark/remark.js
@@ -22,44 +22,17 @@ angular.module('myApp.remark', ['ngRoute'])
             $scope.song = Song.get({
                 id: data.song.id
             })
-        });;
-        /*
-        $scope.remark = Remark.get({
-            id: $routeParams.remarkId
-        }).then(function(data) {
-            console.log("Blabla " + data.song.id)
-            $scope.song = Song.get(data.song.id);
-            // user.abc = true;
-            // user.$save();
-        });*/
-        /*
-        Remark.get({
-            id: $routeParams.remarkId
-        })
-            .$promise.then(function(data) {
-                $scope.remark = data;
-                console.log("Blabla " + data.song);
-                $scope.song = data.song;
-                // $scope.user = user;
-            });*/
-        /*
-        $scope.song = Song.get({
-            id: 12743
-        });*/
+        });
+
 
         $scope.save = function() {
             console.log("Saving song by user " + $cookieStore.get('user'));
-            // $scope.song.userModified = $cookieStore.get('user');
-            // $scope.song.$save();
-            // $location.path('/songs');
-            // console.log("Saving song " + $scope.remark.song.artist + " - " + $scope.remark.song.title);
+            $scope.song.userModified = $cookieStore.get('user');
             $scope.song.$save();
         };
 
         $scope.cancel = function() {
-            // console.log("Canceling...");
-            // $location.path('/remarks');
+            console.log("Canceling...");
         };
-        // console.log("Opening remark details...");
     }
 ]);
