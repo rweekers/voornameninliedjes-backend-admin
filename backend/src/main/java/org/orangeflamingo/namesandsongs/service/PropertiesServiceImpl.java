e1ccf1dc7d007dd95aa216b7341f74f558e1f2e7
Remco Weekers <Remco.Weekers@gmail.com>
2014-12-13 16:25:02 +0100

    Release 0.2.

--- backend/src/main/java/org/orangeflamingo/namesandsongs/service/PropertiesServiceImpl.java
+++ backend/src/main/java/org/orangeflamingo/namesandsongs/service/PropertiesServiceImpl.java
@@ -1,9 +1,7 @@
 package org.orangeflamingo.namesandsongs.service;
 
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.util.Properties;
 
 import org.apache.log4j.Logger;
@@ -14,26 +12,23 @@ import org.springframework.stereotype.Component;
 @Scope("singleton")
 public class PropertiesServiceImpl implements PropertiesService {
 
-    private static final Logger LOGGER = Logger
-            .getLogger(PropertiesServiceImpl.class);
+	private static final Logger LOGGER = Logger
+			.getLogger(PropertiesServiceImpl.class);
 
-    private static Properties props;
+	private static Properties props;
 
-    public PropertiesServiceImpl() throws IOException {
-        LOGGER.info("Creating propertiesService...");
-        // create and load default properties
-        props = new Properties();
-        FileInputStream in = new FileInputStream("/config.properties");
-        LOGGER.info("Properties loaded...");
-        props.load(in);
-        in.close();
+	public PropertiesServiceImpl() throws IOException {
+		LOGGER.info("Creating propertiesService...");
+		// create and load default properties
+		props = new Properties();
+		FileInputStream in = new FileInputStream("/config.properties");
+		LOGGER.info("Properties loaded...");
+		props.load(in);
+		in.close();
+	}
 
-        OutputStream out = new FileOutputStream("/test.txt");
-        out.close();
-    }
-
-    public String get(String key) {
-        return props.getProperty(key);
-    }
+	public String get(String key) {
+		return props.getProperty(key);
+	}
 
 }
