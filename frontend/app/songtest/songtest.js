415c211178166fc320e6bd0ef0b6343250370e86
Remco Weekers <remco.weekers@gmail.com>
2015-07-31 13:40:56 +0200

    Update for testing Spring vs Node.

--- frontend/app/songtest/songtest.js
+++ frontend/app/songtest/songtest.js
@@ -15,7 +15,6 @@ angular.module('myApp.songtest', ['ngRoute', 'ngResource'])
     function($scope, $location, $http, $routeParams, $sce, SongDetail, Data) {
         var start = new Date().getTime();
         var start2 = new Date().getTime();
-        var start3 = new Date().getTime();
 
         $http.get('/namesandsongs/api/song').success(function(data){
             $scope.songs = data;
@@ -24,19 +23,11 @@ angular.module('myApp.songtest', ['ngRoute', 'ngResource'])
             $( ".report" ).append( "<h4>Execution time: </h4>" + time);
             console.log("Time1 " + time);
         });
-        /*
-        $http.get('/namesandsongs/api/song2').success(function(data){
+        $http.get('/node/api/song').success(function(data){
             $scope.songs2 = data;
             var end2 = new Date().getTime();
             var time2 = end2 - start2;
             $( ".report2" ).append( "<h4>Execution time: </h4>" + time2);
-        });*/
-        $http.get('/node/api/song').success(function(data){
-            $scope.songs3 = data;
-            var end3 = new Date().getTime();
-            var time3 = end3 - start3;
-            $( ".report3" ).append( "<h4>Execution time: </h4>" + time3);
-            console.log("Time3 " + time3);
         });
     }
 ]);
