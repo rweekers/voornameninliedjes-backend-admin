2bc7ba7ae26bf49ccac5f41213df2333589803de
Remco Weekers <remco.weekers@gmail.com>
2017-09-29 23:26:48 +0200

    Add test setup

---
+++ src/test/java/nl.orangeflamingo/controller/SongControllerTest.java
@@ -0,0 +1,13 @@
+package nl.orangeflamingo.controller;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class SongControllerTest {
+
+    @Test
+    public void test() {
+        Assert.assertTrue(true);
+    }
+}
