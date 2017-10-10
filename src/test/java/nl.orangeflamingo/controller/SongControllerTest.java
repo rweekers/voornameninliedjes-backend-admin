d687938a65c53bc7af2021e4b5438a40a6dcc3a2
Remco Weekers <remco@craftsmen.nl>
2017-10-10 21:09:22 +0200

    Add tests, format pom.xml

--- src/test/java/nl.orangeflamingo/controller/SongControllerTest.java
+++ src/test/java/nl.orangeflamingo/controller/SongControllerTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
@@ -19,8 +20,20 @@ public class SongControllerTest {
     private SongController songController;
 
     @Test
-    public void test() {
-        List<Song> allSongs = songController.findSongsByArtist("Dolly Parton");
-        assertEquals(1, allSongs.size());
+    public void testFindSongsByArtist() {
+        List<Song> songsByArtist = songController.findSongsByArtist("Dolly Parton");
+        assertEquals(1, songsByArtist.size());
+    }
+
+    @Test
+    public void testFindSongsByTitle() {
+        List<Song> songsByTitle = songController.findSongsByTitle("Jolene");
+        assertEquals(1, songsByTitle.size());
+    }
+
+    @Test
+    public void testFindSongsByPage() {
+        Page<Song> songsByPage = songController.findSongsByPage(0, 1);
+        assertEquals(1, songsByPage.getSize());
     }
 }
