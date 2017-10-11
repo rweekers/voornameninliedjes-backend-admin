eb1b170371c8b3c116473ef6097826e8739b30bd
Remco Weekers <Remco.Weekers@gmail.com>
2017-10-11 10:53:47 +0200

    Add private constructor

--- src/main/java/nl/orangeflamingo/domain/SongSpecs.java
+++ src/main/java/nl/orangeflamingo/domain/SongSpecs.java
@@ -4,6 +4,10 @@ import org.springframework.data.jpa.domain.Specification;
 
 public class SongSpecs {
 
+    private SongSpecs() {
+        // prevent from instantiating
+    }
+    
     public static Specification<Song> songWithArtistOrTitleLike(String queryString) {
         return (Specification<Song>) (root, query, builder) ->
                 builder.or(builder.like(builder.lower(root.get("artist")), getLowercaseQueryWithWildcards(queryString)),
