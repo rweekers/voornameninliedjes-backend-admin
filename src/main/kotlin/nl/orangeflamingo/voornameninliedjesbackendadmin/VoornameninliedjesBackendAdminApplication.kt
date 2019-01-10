46ea5c82db7ec04e45ad63adec6aef71c0bb50a4
Remco Weekers <rweekers@bol.com>
2019-01-10 11:35:32 +0100

    Initial commit reactive backend

---
+++ src/main/kotlin/nl/orangeflamingo/voornameninliedjesbackendadmin/VoornameninliedjesBackendAdminApplication.kt
@@ -0,0 +1,12 @@
+package nl.orangeflamingo.voornameninliedjesbackendadmin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VoornameninliedjesBackendAdminApplication
+
+fun main(args: Array<String>) {
+	runApplication<VoornameninliedjesBackendAdminApplication>(*args)
+}
+
---
+++ src/test/kotlin/nl/orangeflamingo/voornameninliedjesbackendadmin/VoornameninliedjesBackendAdminApplicationTests.kt
@@ -0,0 +1,17 @@
+package nl.orangeflamingo.voornameninliedjesbackendadmin
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class VoornameninliedjesBackendAdminApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
+
