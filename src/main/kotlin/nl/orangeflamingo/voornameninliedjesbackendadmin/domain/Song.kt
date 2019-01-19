628ffebf8da03e9ae23c0ba451f0975a656042e1
Remco Weekers <remco@craftsmen.nl>
2019-01-19 08:41:29 +0100

    Add JsonFormat for parsing dates directly

--- src/main/kotlin/nl/orangeflamingo/voornameninliedjesbackendadmin/domain/Song.kt
+++ src/main/kotlin/nl/orangeflamingo/voornameninliedjesbackendadmin/domain/Song.kt
@@ -1,5 +1,6 @@
 package nl.orangeflamingo.voornameninliedjesbackend.domain
 
+import com.fasterxml.jackson.annotation.JsonFormat
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
 import java.time.Instant
@@ -11,8 +12,13 @@ data class Song(
         val artist: String,
         val title: String,
         val name: String,
+
+        @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSZ", timezone = "Europe/Amsterdam")
         val dateInserted: Instant,
+
+        @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSZ", timezone = "Europe/Amsterdam")
         val dateModified: Instant?,
+
         val userInserted: String,
         val userModified: String?
 )
