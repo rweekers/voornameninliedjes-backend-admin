91fafd19a5138334c497917bf4432bfb487fa14b
Remco Weekers <remco.weekers@gmail.com>
2020-02-01 09:33:59 +0100

    Upgrade to Java 11, disable Sonar Scan for now

--- build.gradle.kts
+++ build.gradle.kts
@@ -68,7 +68,7 @@ sonarqube {
 
 group = "nl.orangeflamingo"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_1_8
+java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
 	mavenCentral()
@@ -92,6 +92,6 @@ dependencies {
 tasks.withType<KotlinCompile> {
 	kotlinOptions {
 		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "1.8"
+		jvmTarget = "11"
 	}
 }
